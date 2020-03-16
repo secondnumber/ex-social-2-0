cb13969ee58843c369365daecb15faa97fa6dac0
Yulya Vorontsova <secondnumber01@gmail.com>
2020-03-16 16:27:34 +0300

    feat(product): add component login page

---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './LoginPage.module.scss';
+
+const LoginPage = () => {
+    return (
+        <div className={classes.wrapper}>
+            <div className={classes.bgImage}></div>
+            <div className={classes.bgSquare}></div>
+        </div>
+    );
+};
+
+export default LoginPage;

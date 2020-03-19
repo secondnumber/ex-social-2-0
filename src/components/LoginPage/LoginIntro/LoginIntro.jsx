cd4b6ed449074e1c51670fdb2d505669b79958ca
Yulya Vorontsova <secondnumber01@gmail.com>
2020-03-19 16:48:26 +0300

    feat(product): add forms and text to login page

--- src/components/LoginPage/LoginIntro/LoginIntro.jsx
+++ src/components/LoginPage/LoginIntro/LoginIntro.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import classes from './LoginIntro.module.scss';
+import Logo from './Logo.png';
 
 const LoginIntro = () => {
-    return <div>LoginIntro</div>;
+    return (
+        <div className={classes.intro}>
+            <img src={Logo} alt="" />
+            <p className={classes.welcome}>WELCOME TO</p>
+            <h1 className={classes.header}>VIKINGER</h1>
+            <p className={classes.text}>
+                The next generation social network & community! Connect with
+                your friends and play with our quests and badges gamification
+                system!
+            </p>
+            <button className={classes.loginButton}>Login</button>
+            <button className={classes.registerButton}>Register</button>
+        </div>
+    );
 };
 
 export default LoginIntro;
--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import classes from './LoginPage.module.scss';
+import LoginIntro from './LoginIntro/LoginIntro';
+import LoginForm from './LoginForm/LoginForm';
 
 const LoginPage = () => {
     return (
         <div className={classes.wrapper}>
-            <div className={classes.bgImage}></div>
+            <div className={classes.bgImage}>
+                <div className={classes.content}>
+                    <LoginIntro />
+                    <LoginForm />
+                </div>
+            </div>
             <div className={classes.bgSquare}></div>
         </div>
     );

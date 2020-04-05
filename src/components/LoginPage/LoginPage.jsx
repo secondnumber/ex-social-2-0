b8cffabd198def38ec3fec7f4346081a18794588
Yulya Vorontsova <secondnumber01@gmail.com>
2020-04-05 21:07:17 +0300

    fix(product): fix problem with switches between routes

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -3,17 +3,21 @@ import classes from './LoginPage.module.scss';
 import LoginIntro from './LoginIntro/LoginIntro';
 import LoginForm from './LoginForm/LoginForm';
 import RegisterForm from './RegisterForm/RegisterForm';
-import {BrowserRouter, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 const LoginPage = () => {
     return (
-        <BrowserRouter>
+        <Router>
         <div className={classes.wrapper}>
             <LoginIntro />
-            <Route path='/register' componenet={RegisterForm}/>
-            <Route path='/login' componenet={LoginForm}/>
+            <Route path="/register">
+                <RegisterForm/>
+            </Route>
+            <Route path="/login">
+                <LoginForm/>
+            </Route>
         </div>
-        </BrowserRouter>
+        </Router>
     );
 };
 

b9337e7053fb9b921ca351f97a360c4d5c010288
Yulya Vorontsova <secondnumber01@gmail.com>
2020-04-07 09:11:48 +0300

    build(components): add eslint with rules ai[Crbnbairbnb rules

--- src/components/LoginPage/LoginIntro/LoginIntro.jsx
+++ src/components/LoginPage/LoginIntro/LoginIntro.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './LoginIntro.module.scss';
 import Logo from './Logo.png';
-import {NavLink} from "react-router-dom";
 
-const LoginIntro = () => {
-    return (
-        <div className={classes.intro}>
-            <img src={Logo} alt="" />
-            <p className={classes.welcome}>WELCOME TO</p>
-            <h1 className={classes.header}>VIKINGER</h1>
-            <p className={classes.text}>
-                The next generation social network & community! Connect with
-                your friends and play with our quests and badges gamification
-                system!
-            </p>
-            <p  className={classes.loginButton}><NavLink to="/login">Login</NavLink></p>
-            <p  className={classes.registerButton}><NavLink to="/register">Register</NavLink></p>
-        </div>
-    );
-};
+const LoginIntro = () => (
+  <div className={classes.intro}>
+    <img src={Logo} alt="" />
+    <p className={classes.welcome}>WELCOME TO</p>
+    <h1 className={classes.header}>VIKINGER</h1>
+    <p className={classes.text}>
+      The next generation social network & community! Connect with
+      your friends and play with our quests and badges gamification
+      system!
+    </p>
+    <p className={classes.loginButton}><NavLink to="/login">Login</NavLink></p>
+    <p className={classes.registerButton}><NavLink to="/register">Register</NavLink></p>
+  </div>
+);
 
 export default LoginIntro;

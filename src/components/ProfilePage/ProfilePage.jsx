112da986dd5516fc371614da0bfb457ef2e69a91
Yulya Vorontsova <secondnumber01@gmail.com>
2020-04-14 19:01:32 +0300

    feat(product): start profile page

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import classes from './ProfilePage.module.scss';
 import Header from './Header/Header/Header';
+import LeftMenu from "./LeftMenu/LeftMenu";
+import ProfileBanner from "./ProfileBanner/ProfileBanner";
 
 const ProfilePage = () => (
-  <div>
-    <Header/>
+  <div className={classes.wrapper}>
+    <Header />
+    <LeftMenu/>
+    <ProfileBanner/>
   </div>
 );
 

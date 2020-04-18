e638145fa0e3453ed46d09f6e05e6e01cc3d295c
Yulya Vorontsova <secondnumber01@gmail.com>
2020-04-18 21:14:19 +0300

    feat(product): start normal grid

--- src/components/ProfilePage/LeftMenu/LeftMenu.jsx
+++ src/components/ProfilePage/LeftMenu/LeftMenu.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import classes from './LeftMenu.module.scss';
+import smallAvatarIcon from './SmallProfileInfo.png';
+import newsFeedIcon from './NewsfeedIcon.png'
 
 const LeftMenu = () => (
-  <div className={classes.header}>
-LeftMenu
+  <div className={classes.wrapper}>
+    <div className={classes.smallAvatar}><a href="#"><img src={smallAvatarIcon} alt=""/></a></div>
+    <div className={classes.newsFeed}><a href=""><img src={newsFeedIcon} alt=""/></a></div>
+    <div>3</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>10</div>
+    <div>11</div>
   </div>
 );
 

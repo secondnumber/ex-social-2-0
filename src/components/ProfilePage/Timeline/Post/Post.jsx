f41cc69f58a4524a5bfdd426354c50659dd69821
Yulya Vorontsova <secondnumber01@gmail.com>
2020-04-25 16:11:24 +0300

    feat(product): add arrays with data

--- src/components/ProfilePage/Timeline/Post/Post.jsx
+++ src/components/ProfilePage/Timeline/Post/Post.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import classes from './Post.module.scss';
 
-const Post = () => (
+const Post = (props) => (
   <article className={classes.wrapper}>
     <div className={classes.header}>
       <div className={classes.info}>
         <img />
-        <p>Marina Valentine</p>
-        <p>5 hours ago</p>
+        <p>{props.author}</p>
+        <p>{props.time}</p>
       </div>
       <button>...</button>
     </div>
     <div className={classes.content}>
       <p>
-        Hi to all! Remember that today I'll be livestreaming along with
-        @NekoBebop the new character of the brand new Xenowatch patch "Anya
-        Darkness". See you there!
+        {props.message}
       </p>
     </div>
     <div className={classes.stats}>
-      <div className={classes.reactions}>12</div>
-      <div className={classes.comments}>4</div>
-      <div className={classes.shares}>1</div>
+      <div className={classes.reactions}>{props.reactions}</div>
+      <div className={classes.comments}>{props.comments}</div>
+      <div className={classes.shares}>{props.shares}</div>
     </div>
     <div className={classes.interactions}>
       <div className={classes.react}>React</div>

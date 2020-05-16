949214b194ccac1978329b17f4ee50d9bfd217b7
Yulya Vorontsova <secondnumber01@gmail.com>
2020-05-16 13:39:46 +0300

    feat(product): start friends page

--- src/components/FriendsPage/FriendsPage.jsx
+++ src/components/FriendsPage/FriendsPage.jsx
@@ -3,12 +3,14 @@ import classes from './FriendsPage.module.scss';
 import HeaderContainer from '../common/Header/HeaderContainer';
 import LeftMenuContainer from '../common/LeftMenu/LeftMenuContainer';
 import RightMenuContainer from '../common/RightMenu/RightMenuContainer';
+import FriendsContainer from './Friends/FriendsContainer';
 
 const FriendsPage = (props) => (
   <div className={classes.wrapper}>
     <HeaderContainer />
     <LeftMenuContainer />
     <RightMenuContainer />
+    <FriendsContainer />
   </div>
 );
 

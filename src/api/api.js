cdfd4bd6a38047ebe2f1961e47635bc0cb81da57
Yulya Vorontsova <secondnumber@Yulyas-MacBook-Air.local>
2020-06-10 13:51:19 +0300

    feat(product): make methods for working with api

---
+++ src/api/api.js
@@ -0,0 +1,26 @@
+import * as axios from 'axios';
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: `https://social-network.samuraijs.com/api/1.0/`,
+  headers: {
+    'API-KEY': 'b1990052-b902-4833-a74d-cf5897729da7',
+  },
+});
+
+export const usersAPI = {
+  getUsers(currentPage = 1, pageSize = 10) {
+    return instance
+      .get(`users?page=${currentPage}&count=${pageSize}`)
+      .then((response) => response.data);
+  },
+  addUser(userId) {
+    return instance.post(`follow/${userId}`).then((response) => response.data);
+  },
+  deleteUser(userId) {
+    return instance.delete(`follow/${userId}`).then((response) => response.data);
+  },
+  isAuthCurrentUser(userId) {
+    return instance.get(`auth/me`).then((response) => response.data);
+  },
+};
